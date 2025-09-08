e5dccaadfe5a809a80bd9e88e8404882a6e88bd7
maridabrins <maridabrins@hotmail.com>
2025-09-08 11:44:39 -0300

    feat: vendo resultado da page Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,16 @@
 import Header from "../../components/Header"
+import Footer from "../../components/Footer"
+import Main from "../../components/PageHome/main"
+import AtividadeSection from "../../components/PageHome/atividade-section"
 
 
 const Home =()=> {
     return(
         <>
         <Header/>
-      
+        <Main/>
+        <AtividadeSection/>
+        <Footer/>
         </>
 
     )
