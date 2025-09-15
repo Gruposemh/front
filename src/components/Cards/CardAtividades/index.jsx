2dff8dfea531f8949574c90416ed68430282dd55
Nicoo200 <nicollass049@gmail.com>
2025-09-15 16:37:29 -0300

    chore: adicionando o component Cards Atividades

---
+++ src/components/Cards/CardAtividades/index.jsx
@@ -0,0 +1,14 @@
+import "../../../styles/Cards/CardAtividades/style.css"
+const CardAtividades = ({image, name}) => {
+    return(
+        <>
+        <div className="card-container">
+            <img className= "icon-card-atividade" src={image} alt={`foto da aula de ${name}`} />
+            <h3>{name}</h3>
+        </div>
+        
+        
+        </>
+    )
+}
+export default CardAtividades
