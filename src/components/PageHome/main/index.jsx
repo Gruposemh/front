90484d3aa358e85b12232a9321e34e91e41b751c
maridabrins@hotmail.com <maridabrins@hotmail.com>
2025-09-16 15:51:57 -0300

    feat: adicionando o Component Button

--- src/components/PageHome/main/index.jsx
+++ src/components/PageHome/main/index.jsx
@@ -1,7 +1,6 @@
 import integrantes from "../../../assets/Home/integrantes.png"
 import "../../../styles/Home/main/style.css"
-import Title from "../../Title";
-import CardAtividades from "../../Cards/CardAtividades"
+import Button from "../../Button";
 
 const Main = () => {
     return(
@@ -11,7 +10,7 @@ const Main = () => {
                     <article className='voluntario'>
                         <h1>Fazer o bem <br /> faz muito <br /> bem!</h1>
                         <p>A ONG Voluntários Torcendo para o Bem atua há anos fazendo o bem para a comunidade local, promovendo eventos e campanhas em prol da população. Venha fazer parte desse movimento!</p>
-                        <button className="btn-primary">Quero ser um voluntário</button>
+                        <Button text="Quero ser um voluntário" primary={true}/>
                       
                     </article>
                     <div className='img-integrantes'>
