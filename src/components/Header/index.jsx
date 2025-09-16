31d0df9d4cfff281ebd58d94b3ee6802c1212e8e
maridabrins@hotmail.com <maridabrins@hotmail.com>
2025-09-16 16:33:03 -0300

    chore: correção da lógica do dropdown

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,63 +1,53 @@
 import "../../styles/Header/style.css"
-import { useState } from "react"
+import { useState } from "react";
 import Logo from "../../assets/Logos/Logo.svg";
 import Button from "../Button";
 
 const Header = () => {
 
-    const [aberto, setAberto] = useState(false); 
-
-
-
-    function DropdownItem(props){
-        return(
-            <nav className="nav-dropdown">
-                <li><a >{props.titulo}</a></li> {/*  href={props.caminho} */ }
-            </nav>
-        
-        )
-    }
-
-   
+     const [aberto, setAberto] = useState(false);
     return(
         <>
         <header>
             <img src={Logo} alt="Logo dos Voluntarios Pro Bem, três bonequinhos, preto cinza e vermelho" className="logo" />
 
-            <nav className="nav-header">
+           
 
-                <ul className="lista-header">
-                    <li><a>Eventos</a></li>
 
-                     
-                        <li onMouseEnter={()=>{setAberto(true)}}
-                         onMouseLeave={()=>{setAberto(false)}}
-                         className="menu-item-com-dropdown">
-                            <a >Sobre Nós</a>
-                            {aberto && (
-                                <div className="sobre-dropdown">
-                                    <ul className="lista-dropdown">
-                                        <DropdownItem titulo={"Nossa História"}/>
-                                        <DropdownItem titulo={"Linha do Tempo"}/>
-                                        <DropdownItem titulo={"Equipe"}/>
-                                        <DropdownItem titulo={"Projetos"}/>
-                                    </ul>
-                                </div>
-                            )}
-                        </li>
 
-                            
 
-                   
 
-                    <li><a>Blog</a></li>
-                    <li><a>Como ajudar</a></li>
-                    
-                </ul>
+    <nav className="nav-header">
+      <ul className="lista-header">
+        <li><a href="#">Eventos</a></li>
 
-               
-                
-            </nav>
+        <li
+          className="menu-item-com-dropdown"
+          onMouseEnter={() => setAberto(true)}
+          onMouseLeave={() => setAberto(false)}
+        >
+          <a href="#">Sobre Nós ▾</a>
+
+          {/* dropdown controlado */}
+          <div className={`sobre-dropdown ${aberto ? "ativo" : "inativo"}`}>
+            <ul className="lista-dropdown">
+              <li><a href="#" className="drop-item">Nossa História</a></li>
+              <li><a href="#" className="drop-item">Linha do Tempo</a></li>
+              <li><a href="#" className="drop-item">Equipe</a></li>
+              <li><a href="#" className="drop-item">Projetos</a></li>
+            </ul>
+          </div>
+        </li>
+
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Como Ajudar</a></li>
+      </ul>
+    </nav>
+  
+
+
+
+
              
             <div className="header-buttons">
                 <Button text= "Doe Agora" primary={true}/>
