de42b286d972564f3a860a521f8a388cf7dfb2a2
maridabrins <maridabrins@hotmail.com>
2025-09-22 14:38:30 -0300

    chore: adicionando scrollbar com o swiper na section atividades

--- src/components/PageHome/atividade-section/index.jsx
+++ src/components/PageHome/atividade-section/index.jsx
@@ -7,35 +7,44 @@ import capoeira from "../../../assets/Home/icon-capoeira.png"
 import danca from "../../../assets/Home/icon-danca.png"
 import muayThai from "../../../assets/Home/icon-muay.png"
 
+import { Swiper, SwiperSlide } from 'swiper/react'
+
 
 
 const AtividadeSection = () => {
+
+    
+  const atividades = [
+    { id: '1', name:"Ballet", image: ballet },
+    { id: '2', name:"Box", image: box },
+    { id: '3', name:"Capoeira", image: capoeira },
+    { id: '4', name:"Muay Thai", image: muayThai },
+    { id: '5', name:"Dança", image: danca },
+   
+  ]
+
     
     return(
         <>
         <section className="atividades">
             <Title title= "Conheça nossas atividades "/>
             <div className="atividades-container">
-                <CardAtividades
-                image = {ballet}
-                name= "Ballet"
-                />
-                <CardAtividades
-                image = {box}
-                name= "Box"
-                />
-                <CardAtividades
-                image = {capoeira}
-                name= "Capoeira"
-                />
-                <CardAtividades
-                image = {muayThai}
-                name= "Muay Thai"
-                />
-                <CardAtividades
-                image = {danca}
-                name= "Dança"
-                />
+                 <Swiper
+                slidesPerView={4}          
+                spaceBetween={20}          
+                pagination={{ clickable: true }}
+                navigation
+                
+            >
+                {atividades.map((atividade) => (
+                    <SwiperSlide key={atividade.id}>
+                        <CardAtividades
+                            image={atividade.image}
+                            name={atividade.name}
+                        />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
             </div>
         </section>
 
