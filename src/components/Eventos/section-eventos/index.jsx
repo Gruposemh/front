7a7a48b80d7276d5f3278550ac5ad82e6644a946
maridabrins@hotmail.com <maridabrins@hotmail.com>
2025-09-29 16:23:21 -0300

    feat: adicionando carrossel de campanhas da página de Eventos

--- src/components/Eventos/section-eventos/index.jsx
+++ src/components/Eventos/section-eventos/index.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import Title from "../../Title"
 import CardEventos  from "../../Cards/CardEventos";
 import "../../../styles/Eventos/section-eventos/style.css";
-import teste from "../../../assets/Eventos/natal.png";
+import natal from "../../../assets/Eventos/evento-natal.svg";
+import pascoa from "../../../assets/Eventos/evento-pascoa.png";
+import arrecadacao from "../../../assets/Eventos/evento-arrecadacao.svg";
+
 
 export default function SectionEventos(){
     return (
         <>
         <section className="section-eventos">
             <Title title={"Eventos"} />
-            <CardEventos img={teste} titulo={"Evento de Natal"} local={"São Mateus, Zona Leste, SP"} data={"20/12/2025"} />
-            <CardEventos img={teste} titulo={"Evento de Natal"} local={"São Mateus, Zona Leste, SP"} data={"20/12/2025"} />
+            <CardEventos img={natal} titulo={"Evento de Natal"} local={"São Mateus, Zona Leste, SP"} data={"20/12/2025"} />
+            <CardEventos img={pascoa} titulo={"Evento de Páscoa"} local={"São Mateus, Zona Leste, SP"} data={"10/02/2025"} />
+            <CardEventos img={arrecadacao} titulo={"Evento de Arrecadação"} local={"São Mateus, Zona Leste, SP"} data={"19/08/2025"} />
         </section>
         </>
     )
