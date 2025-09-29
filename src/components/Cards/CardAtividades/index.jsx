3bdbd400b863393e307453c54b64a6a5f6ad138a
maridabrins@hotmail.com <maridabrins@hotmail.com>
2025-09-29 16:21:33 -0300

    feat: ajustando carrossel da página inicial

--- src/components/Cards/CardAtividades/index.jsx
+++ src/components/Cards/CardAtividades/index.jsx
@@ -2,7 +2,7 @@ import "../../../styles/Cards/CardAtividades/style.css"
 const CardAtividades = ({image, name}) => {
     return(
         <>
-        <div className="card-container">
+        <div className="cardAtiv-container">
             <img className= "icon-card-atividade" src={image} alt={`foto da aula de ${name}`} />
             <h3>{name}</h3>
         </div>
