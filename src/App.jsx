c14c03957e80b8a85c9a020cc3b468e32a73c83a
maridabrins <maridabrins@hotmail.com>
2025-09-29 10:17:12 -0300

    feat: adicionando rotas de navegação

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home";
+import Eventos from "./pages/Eventos";
 
 const App = () => {
   return (
     <>
-    
-    <Home/>
-    
-   
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Eventos/>}/>
+       {/*  <Route path="/" element={<Cadastro/>}/>
+        <Route path="/" element={<ComoAjudar/>}/>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/" element={<SobreNos/>}/>
+        <Route path='/*'element={ <NotFound/> }/> */}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
