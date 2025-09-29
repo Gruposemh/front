ae88a16494ad7c7baa0f522d4b3d2c4b5681ad67
maridabrins <maridabrins@hotmail.com>
2025-09-29 11:33:59 -0300

    feat: utilizando as props do CardEventos

--- src/components/Eventos/section-eventos/index.jsx
+++ src/components/Eventos/section-eventos/index.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Title from "../../Title"
-import Card  from "../../Cards/CardEventos";
-import "../../../styles/Eventos/section-eventos/style.css"
+import CardEventos  from "../../Cards/CardEventos";
+import "../../../styles/Eventos/section-eventos/style.css";
+import teste from "../../../assets/Eventos/natal.png";
 
 export default function SectionEventos(){
     return (
         <>
         <section className="section-eventos">
             <Title title={"Eventos"} />
-            <Card />
-            <Card />
+            <CardEventos img={teste} titulo={"Evento de Natal"} local={"São Mateus, Zona Leste, SP"} data={"20/12/2025"} />
+            <CardEventos img={teste} titulo={"Evento de Natal"} local={"São Mateus, Zona Leste, SP"} data={"20/12/2025"} />
         </section>
         </>
     )
