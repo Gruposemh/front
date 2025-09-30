a74b9b5fed9339b7802484d47372390ed3693783
Thiago-234 <camposthiago2021@gmail.com>
2025-09-30 14:10:11 -0300

    Feat: Adicionando a tela Tarefas completa

--- src/components/Tarefas/section-aulas/index.jsx
+++ src/components/Tarefas/section-aulas/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import CardAula from "../../Cards/CardAulas";
 import teste from "../../../assets/Tarefas/section-agenda/ballet.png"
+import "../../../styles/Tarefa/section-aulas/style.css"
 
-export default function SectionAulas(){
+export default function SectionAulas() {
     return (
         <>
-            <CardAula img={teste} local={"São Mateus, Zona Leste, SP"} data={"Segunda e Terça"}/>
+            <section className="lista-aulas">
+                <CardAula img={teste} local={"São Mateus, Zona Leste, SP"} data={"Segunda e Terça"} />
+                <CardAula img={teste} local={"São Mateus, Zona Leste, SP"} data={"Segunda e Terça"} />
+            </section>
         </>
     )
 }
--- src/components/Tarefas/section-tarefas/index.jsx
+++ src/components/Tarefas/section-tarefas/index.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import CardTarefa from "../../Cards/CardTarefa"
 import "../../../styles/Tarefa/section-tarefas/style.css"
 
-const ListaTarefa = ({ }) => {
+const ListaTarefa = ({ emAgenda = false }) => {
     return(
         <>
-            <section className="lista-tarefas">
-                <CardTarefa nome={"Limpeza da sede"} data={"Segunda à Sexta"}/>
-                <CardTarefa nome={"Zeladoria na praça"} data={"Sábado"}/>
-                <CardTarefa nome={"Ajuda em evento"} data={"23/09/2025"}/>
+            <section className={`lista-tarefas ${emAgenda ? 'em-agenda' : ''}`}>
+                <CardTarefa nome={"Limpeza da sede"} data={"Segunda à Sexta"} emAgenda={emAgenda}/>
+                <CardTarefa nome={"Zeladoria na praça"} data={"Sábado"} emAgenda={emAgenda}/>
+                <CardTarefa nome={"Ajuda em evento"} data={"23/09/2025"} emAgenda={emAgenda}/>
             </section>
         </>
     )
