cc5c6057408695220630b454f10de93e74276900
maridabrins <maridabrins@hotmail.com>
2025-10-02 15:19:22 -0300

    feat: adicionando animações no card da linha do tempo

--- src/components/Cards/Cards-SobreNos/Card/index.jsx
+++ src/components/Cards/Cards-SobreNos/Card/index.jsx
@@ -1,32 +1,17 @@
 import React from "react";
-import "../../../../styles/Cards/Card-SobreNos/Card-tempo/style.css"
+import "../../../../styles/Cards/Card-SobreNos/Card-tempo/style.css";
+import AnimatedSection from "../../../AnimatedSection";
 
 const Card = ({ ano, img, acontecimento, texto, invertido }) => {
     return (
-        <>
-            {
-                invertido == false ? (
-                    <div className="card-sobre-nos">
-                        <div className="ano">
-                            <p>{ano}</p>
-                        </div>
-                        <div className="info-card">
-                            <div>
-                                <img src={img} />
-                            </div>
-                            <div className="linha-card"></div>
-                            <div className="titulo-acontecimento">
-                                <h3>{acontecimento}</h3>
-                                <p>{texto}</p>
-                            </div>
-                        </div>
-                    </div>
-                ) : (
-                    <div className="card-sobre-nos">
-                        <div className="ano">
-                            <p>{ano}</p>
-                        </div>
-                        <div className="info-card">
+        <AnimatedSection delay={0.4}>
+            <div className="card-sobre-nos">
+                <div className="ano">
+                    <p>{ano}</p>
+                </div>
+                <div className="info-card">
+                    {invertido ? (
+                        <>
                             <div className="titulo-acontecimento">
                                 <h3>{acontecimento}</h3>
                                 <p>{texto}</p>
@@ -35,12 +20,23 @@ const Card = ({ ano, img, acontecimento, texto, invertido }) => {
                             <div>
                                 <img src={img} />
                             </div>
-                        </div>
-                    </div>
-                )
-            }
-        </>
-    )
+                        </>
+                    ) : (
+                        <>
+                            <div>
+                                <img src={img} />
+                            </div>
+                            <div className="linha-card"></div>
+                            <div className="titulo-acontecimento">
+                                <h3>{acontecimento}</h3>
+                                <p>{texto}</p>
+                            </div>
+                        </>
+                    )}
+                </div>
+            </div>
+        </AnimatedSection>
+    );
 };
 
 export default Card;
