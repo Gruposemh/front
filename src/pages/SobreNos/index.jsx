8c092731f6e452ff9323ec6f9df0c13b2ba681ea
maridabrins <maridabrins@hotmail.com>
2025-10-02 15:17:57 -0300

    feat: adicionando a section-historia na página sobre nos

--- src/pages/SobreNos/index.jsx
+++ src/pages/SobreNos/index.jsx
@@ -6,12 +6,14 @@ import SectionLinhaTempo from "../../components/SobreNos/section-linha-tempo";
 import SectionPilares from "../../components/SobreNos/section-pilares";
 import SectionEquipe from "../../components/SobreNos/section-equipe";
 import img_sobreNos from "../../assets/SobreNos/sobreNos.png"
+import SectionHistoria from "../../components/SobreNos/section-historia";
 
 export default function SobreNos() {
     return (
         <>
             <Header />
             <Main img={img_sobreNos} text={"Sobre nós"}/>
+            <SectionHistoria/>
             <SectionLinhaTempo />
             <SectionPilares />
             <SectionEquipe />
