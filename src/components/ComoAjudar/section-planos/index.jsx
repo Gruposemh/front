ad50101609506f6b7a86284cd1916f046acd3d31
maridabrins <maridabrins@hotmail.com>
2025-10-02 23:17:13 -0300

    feat: ajustando section-planos

--- src/components/ComoAjudar/section-planos/index.jsx
+++ src/components/ComoAjudar/section-planos/index.jsx
@@ -1,29 +1,40 @@
 import React from "react";
 import "../../../styles/ComoAjudar/section-planos/style.css"
 import Button from "../../../components/Button"
+import AnimatedSection from "../../AnimatedSection";
 
 export default function SectionPlanos() {
     return (
         <>
-            <div className="planos-container">
-                <h1>Fazer o Bem Faz Muito Bem</h1>
-                <div className="planos">
-                    <div className="ser-voluntario">
-                        <h4>Seja um voluntário e nos <br /> ajude em nossas <br /> atividades</h4>
+        <AnimatedSection delay={0.4}>
+            <section className="section-planos">
+                <h2>Fazer o Bem Faz Muito Bem</h2>
+            
+               
+                <div className="planos-container">
+                    <div className="planos-content">
+                        <h3>Seja um voluntário e nos ajude em nossas atividades!</h3>
                         <Button text={"Quero ser um voluntário"} />
                     </div>
-                    <div className="mensal">
-                        <p>Mensal</p>
-                        <h3>Escolha um valor <br /> fixo para doar<br /> mensalmente</h3>
-                        <Button text={"Continue"} />
-                    </div>
+                    <div className="cards-doacao">
+                        <div className="mensal">
+                            <p>Mensal</p>
+                            <h4>Escolha um valor <br /> fixo para doar<br /> mensalmente</h4>
+                            <Button text={"Continue"} />
+                        </div>
                     <div className="doe-agora">
                         <p>Doe agora</p>
-                        <h3>Faça sua doação agora mesmo</h3>
+                        <h5>Faça sua doação agora mesmo</h5>
                         <button>Continue</button>
                     </div>
+                    
                 </div>
+                
+                    
+                
             </div>
+            </section>
+            </AnimatedSection>
         </>
     )
 }
