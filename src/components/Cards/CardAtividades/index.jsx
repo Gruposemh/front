f2b81707155a82da7307ab974d508067d788088f
Nicoo200 <nicollass049@gmail.com>
2025-10-06 11:12:42 -0300

    feat: adicionando modal de inscrição em atividade

--- src/components/Cards/CardAtividades/index.jsx
+++ src/components/Cards/CardAtividades/index.jsx
@@ -1,16 +1,13 @@
 import "../../../styles/Cards/CardAtividades/style.css"
-const CardAtividades = ({ image, name }) => {
-    return (
-        <>
 
-        <div className="cardAtiv-container">
-            <img className= "icon-card-atividade" src={image} alt={`foto da aula de ${name}`} />
-            <h3>{name}</h3>
-        </div>
-        
-        
-
-        </>
-    )
-}
+const CardAtividades = ({ image, name, onClick, data, horario }) => {
+  return (
+    <div className="cardAtiv-container" onClick={onClick}>
+      <img className="icon-card-atividade" src={image} alt={`foto da aula de ${name}`} />
+      <h3>{name}</h3>
+      <p>{data}</p>
+        <p>{horario}</p>
+    </div>
+  );
+};
 export default CardAtividades
