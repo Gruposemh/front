4bf76fe95b5fbb6bafd39288990606e65f03139f
Nicoo200 <nicollass049@gmail.com>
2025-10-27 11:47:27 -0300

    feat: ajustando modal de atividades

--- src/components/Modais/ModalAtividades/index.jsx
+++ src/components/Modais/ModalAtividades/index.jsx
@@ -1,15 +1,36 @@
+import { useEffect, useRef } from "react";
 import Button from "../../Button";
-import "../../../styles/Modais/modalAtividades/style.css"
+import "../../../styles/Modais/modalAtividades/style.css";
 
 const ModalAtividades = ({ aula, data, horario, isOpen, onClose, position }) => {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        onClose();
+      }
+    }
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen, onClose]);
+
   if (!isOpen || !position) return null;
 
   const style = {
-    position: "absolute",       
+    position: "absolute",
     top: position.top + "px",
     left: position.left + "px",
     width: position.width + "px",
-    padding: "20px 30px",
+    padding: "34px 32px",
     background: "#fff",
     textAlign: "center",
     borderRadius: "15px",
@@ -18,14 +39,13 @@ const ModalAtividades = ({ aula, data, horario, isOpen, onClose, position }) => 
   };
 
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" style={style} onClick={e => e.stopPropagation()}>
-        <h3>{aula}</h3>
-        <p>{data}</p>
-        <p>{horario}</p>
-        <Button text="Inscrever-se" />
-      </div>
+    <div ref={modalRef} className="ativCard-container" style={style}>
+      <h3>{aula}</h3>
+      <p>{data}</p>
+      <p>{horario}</p>
+      <Button text="Inscrever-se" />
     </div>
   );
 };
+
 export default ModalAtividades;
